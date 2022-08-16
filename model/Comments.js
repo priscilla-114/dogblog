5d72585c273a8bedfe0bd177a135c4be04fb804c
Zaidi <zaidiahsan.r@gmail.com>
2022-08-16 15:22:49 -0500

    Comments.js

---
+++ model/Comments.js
@@ -0,0 +1,50 @@
+//Grab all necessary connecting files
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require('../config/connection');
+
+//Make Comment class and Create Schema for Comments
+class Comment extends Model {}
+
+//Blueprint for the Comments table including all elements for comments
+//Pulling User and Post id to link with the Comments Table
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2],
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "Users",
+                key: "id",
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "Post",
+                key: "id"
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "Comments",
+    }
+);
+
+//exporting the file
+module.exports= Comments;
