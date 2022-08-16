cb5c1a5432746946869888305a3e0505cc13b213
Zaidi <zaidiahsan.r@gmail.com>
2022-08-16 18:20:56 -0500

    single user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,3 +13,31 @@ router.get("/", (req, res) => {
         res.status(500).json(err);
     });
 });
+
+//Route to get a single Users profile
+router.get("/:id", (req, res) => {
+    Users.findOne({
+        attributes: { exclude: ["password"] },
+        where: {
+            id:req.params.id,
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ["id", "title", "content"],
+            },
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id"],
+            },
+        ]
+    }).then((userInfo) => {
+        if (!userInfo) {
+            res.status(404).json({ message: "No user found matching this id" });
+        }
+        res.json(userInfo);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
