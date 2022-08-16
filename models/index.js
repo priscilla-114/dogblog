395c88e77df9729600ac12f9dd273f3c15ce06bd
Zaidi <zaidiahsan.r@gmail.com>
2022-08-16 14:38:53 -0500

    Post relationships

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Comments = require('./Comments');
 const Likes = require('./Likes');
 const Post = require('./Post');
 
+//PRIMARY USER RELATIONSHIPS 
 //creating a one to many relationship with Users and post
 Users.hasMany(Post, {
     foreignKey: 'user_id',
@@ -15,11 +16,31 @@ Post.belongsTo(Users, {
 //creating one to many relationship with users and comments
 Users.hasMany(Comments, {
     foreignKey: 'user_id',
+    onDelete: 'cascade',
 });
 //creating a one to one relationship with the comments and users
 Comments.belongsTo(Users, {
     foreignKey: 'user_id',
     onDelete: 'cascade',
 });
+//creating one to many relationship with Users and likes
+Users.hasMany(Likes, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+});
+//creating a one to one relationship with likes and users
+Likes.belongsTo(Users, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+});
 
+//C
+Post.hasMany(Likes, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+});
 
+Likes.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+});
