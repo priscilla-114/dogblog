462e7a2294eb12b0fb80cc637ee91e8d6b61129b
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-08-16 23:00:22 -0500

    edit/delete post

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,20 @@
+async function editFormHandler(event) {
+
+    event.preventDefault();
+}
+
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+
+
+
+
+
+
+
+<script src="/javascript/edit-post"></script>;
+
+<script src="/javascript/delete-post.js"></script>;
+
+<script src="/javascript/comment.js"></script>
+
