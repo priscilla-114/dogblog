864973d84d8609f754681a0e3f3d0f27e871fc73
Zaidi <zaidiahsan.r@gmail.com>
2022-08-16 20:48:27 -0500

    export

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -15,4 +15,38 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.session.post_id,
+        }).then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+router.delete("/:id", (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id,
+        },
+    }).then((dbCommentData) => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: "No comment found matching this id" });
+            return;
+        }
+        res.json(dbCommentData);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);  
+    });
+});
+
+module.exports= router;
+
+
 
