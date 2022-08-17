73390258f33e500d560a6894929df85a4b359531
Zaidi <zaidiahsan.r@gmail.com>
2022-08-17 12:53:48 -0500

    FindOne Post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -27,5 +27,30 @@ router.get("/", (req, res) => {
     });
 });
 
-
+//Finding One specific post
+router.get("/:id", (req, res) => {
+    Post.findOne({
+        where: { id: req.params.id },
+        attributes: ["id", "title", "content", "user_id"],
+        include: [
+            {
+                model: Comments,
+                attributes: ["id", "comment_text", "post_id", "user_id"],
+                include: {
+                    model: Users,
+                    attributes: ["username"],
+                },
+            },
+        ],
+    }).then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found matching this id" });
+            return;
+        }
+        res.json(dbPostData)
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
