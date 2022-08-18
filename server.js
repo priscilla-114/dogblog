f8bc30bcb39737a1569e960d9951c27ca362c9f8
Zaidi <zaidiahsan.r@gmail.com>
2022-08-18 12:38:00 -0500

    user seeds

--- server.js
+++ server.js
@@ -13,3 +13,21 @@ const hbs = exphbs.create({});
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Statics assets directory to images, css and front-end js
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Router to api folder
+app.use(require('./controllers/'));
+
+//TODO: Uncomment section below once seeds and models are complete
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+});
