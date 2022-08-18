1497b527cf2eb2d6331130a13c6e4cd668e3b99e
Zaidi <zaidiahsan.r@gmail.com>
2022-08-18 14:33:32 -0500

    index

---
+++ seeds/index.js
@@ -0,0 +1,6 @@
+const seedComments = require("./comment-seeds");
+const seedUsers = require("./user-seeds");
+const seedPosts = require("./post-seeds");
+const seedUserlikes = require("./likes-seeds");
+
+const sequelize = require("../config/connection");
